b01f4fbe6c4f9f7e5ec1b208cf46603f5f1c1f5f
Jurjen de Haan <j.dehaan-3@student.utwente.nl>
2025-06-13 21:15:48 +0200

    Added training part of the pipeline

--- src/utilities/mutual_information.py
+++ src/utilities/mutual_information.py
@@ -1,8 +1,9 @@
-from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import mutual_info_regression
 
 
 def mi_score(feature1, feature2):
-    return mutual_info_classif(feature1, feature2)
+    return mutual_info_regression(feature1.values.reshape(-1, 1), feature2, discrete_features='auto')[0] #not truly symmetric, but also used by the authors,
+    # feature 2 is here seen as a 'target', hence no only feature1 is reshaped
 
 def MI(features, X):
     """
@@ -12,10 +13,10 @@ def MI(features, X):
     """
     feature_pairs = {}
     mi_pairs = []
-    for i,j in features: #features should be a list of tuples of every possible combination
-        mi_scores = mi_score(X[i], X[j])
+    for f1, f2 in features: #features should be a list of tuples of every possible combination
+        mi_scores = mi_score(X[f1], X[f2])
         score = mi_scores[0]
-        feature_pairs[score] = (i,j) #key is score, value is pairs of features
+        feature_pairs[score] = (f1, f2) #key is score, value is pairs of features
         #potential problem is that score may be overwritten, leading to data loss
     p = dict(sorted(feature_pairs.items(), reverse=True)) #sort the dict feature_pairs
     for i in p.keys():
