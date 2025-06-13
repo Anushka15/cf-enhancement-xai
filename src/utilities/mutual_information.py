cc73d68a1b8265e3df09a6374b03fa25e3fdef18
Jurjen de Haan <j.dehaan-3@student.utwente.nl>
2025-06-13 21:23:49 +0200

    MI seems to be done and tested

--- src/utilities/mutual_information.py
+++ src/utilities/mutual_information.py
@@ -3,7 +3,7 @@ from sklearn.feature_selection import mutual_info_regression
 
 def mi_score(feature1, feature2):
     return mutual_info_regression(feature1.values.reshape(-1, 1), feature2, discrete_features='auto')[0] #not truly symmetric, but also used by the authors,
-    # feature 2 is here seen as a 'target', hence no only feature1 is reshaped
+    # feature 2 is here seen as a 'target', hence only feature1 is reshaped
 
 def MI(features, X):
     """
@@ -15,7 +15,7 @@ def MI(features, X):
     mi_pairs = []
     for f1, f2 in features: #features should be a list of tuples of every possible combination
         mi_scores = mi_score(X[f1], X[f2])
-        score = mi_scores[0]
+        score = mi_scores
         feature_pairs[score] = (f1, f2) #key is score, value is pairs of features
         #potential problem is that score may be overwritten, leading to data loss
     p = dict(sorted(feature_pairs.items(), reverse=True)) #sort the dict feature_pairs
