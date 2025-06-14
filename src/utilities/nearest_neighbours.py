f1dff76a53d3fff00907326ce0121089aab3154b
Jurjen de Haan <j.dehaan-3@student.utwente.nl>
2025-06-14 13:42:34 +0200

    MI seems to be done and tested

--- src/utilities/nearest_neighbours.py
+++ src/utilities/nearest_neighbours.py
@@ -3,11 +3,11 @@ from scipy.spatial import KDTree
 
 def FNN(desired_space,x,radius):
     """
-    Creates a KDTree object for finding the nearest neighbour efficiently
-    param desired_space: consider only the partitions from the desired neighbourhood from user constraints
-    param x: instance for which we want to find neighbours
+    Creates a KDTree object for finding the region for perturbations
+    param desired_space: the feature space to consider (a collection of feature vectors)
+    param x: instance for which we want to find neighbours/perturbations
     param radius: to measure the distance from x to a point
-    returns: the indices of the nearest neighbours found
+    returns: the indices of the nearest neighbours found, these are possible perturbations, not the final ones, because MI is used for that as well.
     """
     tree = KDTree(desired_space)
     idx = tree.query_ball_point(x,r=radius)
@@ -15,17 +15,17 @@ def FNN(desired_space,x,radius):
     nn = pd.DataFrame.from_records(nn, columns=x.columns)
     return nn
 
-def intervals(nn, perturb_map, f2change, x):
+def intervals(nn, p, f2change, x):
     """
-    param nn:
-    param perturb_map:
-    param radius: to restrict the neighbourhood around x
+    Define a subspace (upper and lower bounds) according to user set constraints (p), possible perturbations (nn) and user set constraints (f2change)
+    param nn: possible perturbations
+    param p: dictionary with features with lower and upper bound of the user-specified interval for ith feature. (e.g. income range)
     returns: subspace
     """
     subspace = {}
-    for i in perturb_map:
+    for i in p:
         lower = p[i][0]
-        upper = p[i][0]
+        upper = p[i][1]
         if upper >= nn[i].max():
             subspace[i] = [lower, nn[i].max()]
         elif lower <= nn[i].min():
