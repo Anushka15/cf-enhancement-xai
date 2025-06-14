0e20823ef5276feb9fefefcde5b6425e50dd3603
Jurjen de Haan <j.dehaan-3@student.utwente.nl>
2025-06-14 16:14:50 +0200

    implemented first version of single feautre CF

---
+++ src/utilities/UFCE_sf.py
@@ -0,0 +1,31 @@
+
+
+def SF(x,cat_f,p,f,t,step):
+    """
+    param x: instance to be explained with CF
+    param cat_f: categorical features
+    param p: user defined constrained features
+    param f: black box model
+    param t: target outcome
+    param step: steps in how to traverse p lower and upper bounds
+    returns: z, a valid countefactual instance
+    """
+    for i in p: #go over each feature in user defined constrained features
+        start = p[i][0] #lower bound
+        end = p[i][1] #upper bound
+        if i not in cat_f: #if i is not a cat feature do binary search to find the minimum value mid such that changing feature i value of x to mid will result in target outcome t and a plausible explanation z
+            while start <= end: #binary search
+                tempdf = x.copy()
+                mid = start + (end-start)/2
+                tempdf.loc[:,i] = mid
+                if f(tempdf) = t and plausible(tempdf): #plausability through outlier detection algorithm LOF (to implement)
+                    z = tempdf
+                    end = mid - step[i] # try to make feature value smaller
+                else:
+                    start = mid + step[i] # too small, make feature value larger
+        else: #if i is categorical
+            z = x
+            z.loc[:,i] = (1-end) #set feature i as the reverse value (check if this works with one-hot encoding)
+            if f(z) = t and plausible(z):
+                return z
+    return z
