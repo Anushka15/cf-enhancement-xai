6ab1ef54fd02569d3685f6211a6d5b36d9249b55
Jurjen de Haan <j.dehaan-3@student.utwente.nl>
2025-06-14 14:54:48 +0200

    added documentation to desired_space.py

--- src/utilities/desired_space.py
+++ src/utilities/desired_space.py
@@ -1,5 +1,11 @@
 import pandas as pd
 def desired_space(desired_output, data): #entire data so x and y
+    """
+    Creates feature space of instances with desired output
+    param desired_output: y, either 1 or 2
+    param data: dataframe including input X and output y
+    returns: feature space of only feature vectors with y=desired_output
+    """
     space = pd.DataFrame()
     space = data[data['class'] == desired_output] #filter instances if they have the desired y
     space = space.drop(['class'], axis=1) #drop y
