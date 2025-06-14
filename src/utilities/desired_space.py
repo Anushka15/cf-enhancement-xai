c50403e2d9df3e259c45a10612c8e2e6b6d8280d
Jurjen de Haan <j.dehaan-3@student.utwente.nl>
2025-06-14 20:40:57 +0200

    changed one-hot encoding from int to bool

--- src/utilities/desired_space.py
+++ src/utilities/desired_space.py
@@ -6,7 +6,7 @@ def desired_space(desired_output, data): #entire data so x and y #the authors do
     param data: dataframe including input X and output y
     returns: feature space of only feature vectors with y=desired_output
     """
-    space = pd.DataFrame()
+    #space = pd.DataFrame()
     space = data[data['class'] == desired_output] #filter instances if they have the desired y
     space = space.drop(['class'], axis=1) #drop y
     return space
